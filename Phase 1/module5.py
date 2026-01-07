116746f6ae58ac9e7295a0c752bfbfb53e60ad2f
Bhavi Sankhala <bsankhala@intelligentdx.ai>
2026-01-07 20:27:40 +0530

    module 4 complete

---
+++ Phase 1/module5.py
@@ -0,0 +1,23 @@
+
+name = input("Enter your name: ")
+
+age = input("Enter your age: ")
+
+city = input("Enter your city name: ")
+
+
+print(f"Your name is {name}, you are {age} years old, and you live in {city}.")
+
+
+salary = float(input("Enter your expected monthly salary: "))
+print(f"Your monthly salary is {salary:.2f}")
+
+
+yearly_salary = salary * 12
+print(f"Your yearly salary will be approximately ₹{yearly_salary:.2f}")
+
+
+print("Example of newline:\n \\n creates a new line")
+print("Example of tab:\t \\t adds a tab space")
+print("Example of backslash: C:\\Users\\{name}\\Documents")
+
